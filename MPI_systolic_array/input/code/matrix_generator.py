790c682346366089730c1461e7664cacc2732847
carlomattioli <carlo.mattioli02@gmail.com>
2025-07-03 09:55:56 +0200

    Update matrix_generator.py

--- MPI_systolic_array/input/code/matrix_generator.py
+++ MPI_systolic_array/input/code/matrix_generator.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-# Genera due matrici 16x16 di tipo float64 (double precision)
+# Generates two matrices with float64 (double precision) data
 matrix1 = np.random.uniform(0.0, 100.0, size=(2000, 2000)).astype(np.float64)
 matrix2 = np.random.uniform(0.0, 100.0, size=(2000, 2000)).astype(np.float64)
 
-# Salva le matrici in file CSV con 3 decimali
+# Saves them in file .csv with 3 decimal digits
 np.savetxt("matrice_A_2000.csv", matrix1, delimiter=",", fmt="%.3f")
 np.savetxt("matrice_B_2000.csv", matrix2, delimiter=",", fmt="%.3f")
